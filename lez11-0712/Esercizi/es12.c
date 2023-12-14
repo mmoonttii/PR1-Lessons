039905258ab7f77a2d71d07b3fe279ace1877a7e
mmoonttii <lori.monti04@gmail.com>
2023-12-14 11:35:53 +0100

    Esercizi 14/12
    Esercizio 11: sovrapposizione di intervalli
    
    Signed-off-by: mmoonttii <lori.monti04@gmail.com>

---
+++ lez11-0712/Esercizi/es12.c
@@ -0,0 +1,12 @@
+/*  Scrivere un programma in cui è dichiarato un vettore v, di dimensione N. Il vettore deve essere popolato con numeri
+ *  interi, positivi e negativi, generati casualmente. Scrivere poi una funzione che riceve come parametro il vettore e
+ *  inverte l' ordine degli elementi. Visualizzare dal main sia il vettore originale che quello ottenuto dopo la
+ *  chiamata della funzione.
+ */
+
+#include <stdio.h>
+
+int main(){
+
+    return 0;
+}
