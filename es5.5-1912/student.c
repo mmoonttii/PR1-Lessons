4f8a65c766c56264e1fbd65956ce55887b4397cb
mmoonttii <lori.monti04@gmail.com>
2023-12-19 15:02:27 +0100

    Es 1/2

--- es5.5-1912/student.c
+++ es5.5-1912/student.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
 
 /** ESERCIZIO 1 [4pt]
  * Definire un'enumerazione Genere, rappresentante i possibili generi di una serie tv, avente i seguenti valori:
  * - AZIONE, COMMEDIA, DOCUMENTARIO, DRAMMATICO, HORROR, ROMANTICO, THRILLER.
  * Definire, poi, una struttura SerieTv avente come campi titolo, anno, numero di stagioni,numero di episodi totali, genere e la valutazione.
  */
+#define LEN 31
+#define VAL_MIN 0
+#define VAL_MAX 10
 
 typedef enum{AZIONE,
 			 COMMEDIA,
@@ -12,13 +19,28 @@ typedef enum{AZIONE,
 			 DRAMMATICO,
 			 HORROR,
 			 ROMANTICO,
-			 THRILLER} Genere
-			 ;
+			 THRILLER} Genere;
 
+typedef struct {
+	char titolo[LEN + 1];
+	int anno;
+	int nStagioni;
+	int nEpisodi;
+	Genere genere;
+	int valutazione;
+} SerieTV;
+
+SerieTV acquisisciSerieTv();
+
+int randRange(int min, int max);
 
 int main()
 {
     // dichiarazione variabili
+
+	// Inserito studente
+	srand(time(NULL));
+
     SerieTV *catalogo = NULL;
     int nSerie = 0;
 
@@ -50,7 +72,84 @@ int main()
  * - il campo genere deve essere un valore valido dell'enumerazione Genere
  * - il campo valutazione deve essere un numero compreso tra 0 e 10 generato casualmente
  **/
-// Inserire codice studente...
+SerieTV acquisisciSerieTv(){
+	SerieTV serieTv;
+	bool flag = true;
+
+	//  Acquisisco campo titolo
+	do {
+		printf("Titolo serie Tv");
+		scanf(" %[^\n]31s", serieTv.titolo);
+
+		flag = strlen(serieTv.titolo) == 0;
+		if (flag) {
+			printf("Il titolo della serie non può essere vuoto, riprovare\n");
+		}
+	} while (flag); //  Il campo titolo non può essere vuoto
+
+	//  Acquisisco campo anno
+	do {
+		printf("Anno serie Tv");
+		scanf("%d", &serieTv.anno);
+
+		flag = serieTv.anno < 1900 || serieTv.anno > 2020;
+		if (flag) {
+			printf("L'anno deve essere compreso tra 1900 e 2020, riprovare\n");
+		}
+	} while (flag); //  Il campo anno deve essere compreso tra 1900 e 2020
+
+	//  Acquisisco campo nStagioni
+	do {
+		printf("Numero stagioni serie Tv");
+		scanf("%d", &serieTv.nStagioni);
+
+		flag = serieTv.nStagioni < 1 || serieTv.nStagioni > 20;
+		if (flag) {
+			printf("Il numero di stagioni deve essere compreso tra 1 e 20, riprovare\n");
+		}
+	} while (flag); //  Il campo nStagioni deve essere compreso tra 1 e 20
+
+	//  Acquisisco campo nEpisodi
+	do {
+		printf("Numero episodi serie Tv");
+		scanf("%d", &serieTv.nEpisodi);
+
+	flag = serieTv.nEpisodi < 1 || serieTv.nEpisodi > 100;
+		if (flag) {
+			printf("Il numero di episodi deve essere compreso tra 1 e 100, riprovare\n");
+		}
+	} while (flag); //  Il campo nEpisodi deve essere compreso tra 1 e 100
+
+	//  Acquisisco campo genere
+	do {
+		printf("Genere serie Tv\n"
+			   "[0] Azione\n"
+			   "[1] Commedia\n"
+		       "[2] Documentario\n"
+		       "[3] Drammatico\n"
+		       "[4] Horror\n"
+		       "[5] Romantico\n"
+		       "[6] Thriller\n");
+		scanf("%d", &serieTv.genere);
+
+		flag = serieTv.genere < 0 || serieTv.genere > 6;
+		if (flag) {
+			printf("Il genere deve essere compreso tra 0 e 6, riprovare\n");
+		}
+	} while (flag); // Il campo genere deve essere compreso tra 0 e 6
+
+	//  Acquisisco campo valutazione
+	serieTv.valutazione = randRange(VAL_MIN, VAL_MAX);
+}
+
+int randRange(int min, int max){
+	int result;
+
+	result = min + rand() % (max - min + 1);
+
+	return result;
+}
+
 /** FINE ESERCIZIO 2 */
 
 
