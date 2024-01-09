3c8f460af1ff5cf8ee5c2e255cb6e9144f0baef8
mmoonttii <lori.monti04@gmail.com>
2024-01-09 15:38:46 +0100

    Lez. 15 - 09/01
    Step 1 - Lettura e stampa files

--- lez15-0901/main.c
+++ lez15-0901/main.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define LEN_STR 31
+
+typedef struct {
+	char nome[LEN_STR + 1];
+	char cpu[LEN_STR + 1];
+	int mp;
+} Smartphone;
+
 
 int main() {
-	printf("Hello, World!\n");
+	Smartphone sp;
+	int read = 0;
+
+	FILE *f = NULL;
+	f = fopen("../smartphone.txt", "r");
+	if (f == NULL) exit(EXIT_FAILURE);
+
+	do {
+		read = 0;
+
+		read += fscanf(f, " %[^\n]31s", sp.nome);
+		read += fscanf(f, " %[^\n]31s", sp.cpu);
+		read += fscanf(f, " %d", &sp.mp);
+
+		if (read == 3) {
+			printf("\nNome: %s"
+			       "\nCpu: %s"
+			       "\nMp: %d\n",
+			       sp.nome, sp.cpu, sp.mp);
+		}
+	} while (read == 3);
+
+	fclose(f);
 	return 0;
 }
